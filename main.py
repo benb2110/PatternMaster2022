854479485f20367e66d1a694eb37f64f1dc5a99d
Ben Beresford <benb2110@gmail.com>
2022-08-24 20:42:16 +1000

    updates

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from pygame.locals import *
 class imagedisplay(pygame.sprite.Sprite):
     def __init__(self, pos, size, s_image):
         super(imagedisplay, self).__init__()
-        self.surf = pygame.image.load("Sprites/" + s_image).convert()
+        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + s_image).convert(), size)
         self.rect = self.surf.get_rect()
+        self.rect.center = pos
+
+    def updateimage(self, newimage):
+        pos = self.rect.center
+        size = self.rect.size
+        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + newimage).convert(), size)
         self.rect.center = pos
 
 
@@ -25,12 +31,36 @@ class textdisplay(pygame.sprite.Sprite):
         self.rect.center = pos
 
 
+class Button:
+    def __init__(self, pos, size, text):
+        self.background = imagedisplay(pos, size, "Button.png")
+        self.text = textdisplay(pos, text)
+
+    def button_check_click(self, pos):
+        if self.background.rect.collidepoint(pos[0], pos[1]):
+            self.action()
+
+
+
+    def add_to_render(self, spritelist):
+        spritelist.append(self.background)
+        spritelist.append(self.text)
+
+
+
 def centeraround(images, pos, padding):
     currentx = -(len(images) * images[0].rect.width + (len(images) - 1) * padding) / 2.0 + pos[0]
     for image in images:
         image.rect.x = currentx
         image.rect.centery = pos[1]
         currentx += images[0].rect.width + padding
+
+
+def testaction():
+    print("Success!!")
+
+
+
 
 
 def main():
@@ -46,21 +76,49 @@ def main():
 
     #first sprites
     playeractions = []
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
-    playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
+    for i in range(7):
+        playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
 
-    centeraround(playeractions, (screen.get_size()[0]/2, screen.get_size()[1]/2), 20)
+    enemyactions = []
+    for i in range(7):
+        enemyactions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
+
+
+    playerchoices = []
+    for i in range(6):
+        playerchoices.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
+
+
+    playerhp = textdisplay((180, 800), "Player HP: " + "100")
+    enemyhp = textdisplay((1400, 800), "Enemy HP: " + "100")
+    enemyname = textdisplay((1355, 765), "Gregore")
+    playerimage = imagedisplay((180, 680), (200, 200), "Warrior.png")
+    enemyimage = imagedisplay((1400, 680), (200, 200), "Warrior.png")
+
+    centeraround(playeractions, (screen.get_size()[0]/2, 600), 20)
+    centeraround(playerchoices, (screen.get_size()[0]/2, 800), 20)
+    centeraround(enemyactions, (screen.get_size()[0]/2, 400), 20)
 
     spritelist = []
-    spritelist.append(textdisplay((800, 800), "Banano"))
+    spritelist.append(playerhp)
+    spritelist.append(enemyhp)
+    spritelist.append(enemyname)
+    spritelist.append(playerimage)
+    spritelist.append(enemyimage)
+
+
     for i in playeractions:
         spritelist.append(i)
+    for i in enemyactions:
+        spritelist.append(i)
+    for i in playerchoices:
+        spritelist.append(i)
 
+    buttonlist = []
+    nextturn = Button((500, 500), (150, 40), "Next turn")
+    buttonlist.append(nextturn)
+    nextturn.add_to_render(spritelist)
+    nextturn.action = testaction
 
     # Display some text
     font = pygame.font.Font(None, 36)
@@ -73,14 +131,20 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
+
     # Event loop
     while 1:
         for event in pygame.event.get():
             #print(event)
             if event.type == QUIT:
                 return
-            if event.type == MOUSEBUTTONDOWN:
-                spritelist[0].updatetext("BANANA")
+            if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                mousepos = pygame.mouse.get_pos()
+                for button in buttonlist:
+                    button.button_check_click(mousepos)
+
+            if event.type == KEYDOWN and event.key == K_h:
+                playerimage.updateimage("WarriorHat.png")
         background.fill((250, 250, 250))
         for sprite in spritelist:
             background.blit(sprite.surf, sprite.rect)
