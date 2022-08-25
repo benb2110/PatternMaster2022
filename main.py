0ce2c8b2f84690c7cc3c51fb68fa3556dac393bb
Ben Beresford <benb2110@gmail.com>
2022-08-25 12:52:51 +1000

    updates

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import pygame
 from pygame.locals import *
+from functools import partial
+
+action_queue = []
+enemy_action_queue = [3, 0, 0, 3, 0, 0, 0]
+playeractions = []
+enemyactions = []
+buttonlist = []
+spritelist = []
+action_names = ["Quick Attack", "Normal Attack", "Heavy Attack", "Dodge", "Block", "Parry", "Heal"]
+revealed_attacks = []
+enemy = None
+player_reveals = 2
+
+
+colour_images = ["Green.png", "Blue.png", "Red.png", "Green.png", "Blue.png", "Red.png"]
+shape_images = ["Circle.png", "Circle.png", "Circle.png", "Triangle.png", "Triangle.png", "Triangle.png"]
+icons = ["Quick Attack.png", "Normal Attack.png", "Heavy Attack.png", "Dodge.png", "Parry.png", "Block.png"]
+
+
 
 
 class imagedisplay(pygame.sprite.Sprite):
     def __init__(self, pos, size, s_image):
         super(imagedisplay, self).__init__()
-        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + s_image).convert(), size)
+        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + s_image).convert_alpha(), size)
         self.rect = self.surf.get_rect()
         self.rect.center = pos
 
     def updateimage(self, newimage):
         pos = self.rect.center
         size = self.rect.size
-        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + newimage).convert(), size)
+        self.surf = pygame.transform.scale(pygame.image.load("Sprites/" + newimage).convert_alpha(), size)
         self.rect.center = pos
 
 
@@ -47,6 +66,39 @@ class Button:
         spritelist.append(self.text)
 
 
+class actiondisplay():
+    def __init__(self, pos, size):
+        self.background = imagedisplay(pos, size, "Button.png")
+        reducedsize = (size[0]-8, size[1]-8)
+        self.colourlayer = imagedisplay(pos, reducedsize, "Grey.png")
+        self.shapelayer = imagedisplay(pos, reducedsize, "Blank.png")
+        self.iconlayer = imagedisplay(pos, reducedsize, "Blank.png")
+        self.rect = self.background.rect
+
+
+    def button_check_click(self, pos):
+        if hasattr(self, 'action') and self.rect.collidepoint(pos[0], pos[1]):
+            self.action()
+
+    def updateposition(self):
+        self.background.rect = self.rect
+        smallrect = Rect(self.rect.left + 4, self.rect.top + 4, self.rect.width - 8, self.rect.height -8)
+        self.colourlayer.rect = smallrect
+        self.shapelayer.rect = smallrect
+        self.iconlayer.rect = smallrect
+
+    def updateimages(self, colour, shape, icon):
+        self.colourlayer.updateimage(colour)
+        self.shapelayer.updateimage(shape)
+        self.iconlayer.updateimage(icon)
+
+
+    def add_to_render(self, spritelist):
+        spritelist.append(self.background)
+        spritelist.append(self.colourlayer)
+        spritelist.append(self.shapelayer)
+        spritelist.append(self.iconlayer)
+
 
 def centeraround(images, pos, padding):
     currentx = -(len(images) * images[0].rect.width + (len(images) - 1) * padding) / 2.0 + pos[0]
@@ -56,10 +108,69 @@ def centeraround(images, pos, padding):
         currentx += images[0].rect.width + padding
 
 
+class character():
+    def __init__(self, max_hp):
+        self.action_queue = []
+        self.hp = max_hp
+        self.max_hp = max_hp
+
+
+
 def testaction():
     print("Success!!")
 
 
+def update_player_action_display():
+    for i in range(len(playeractions)):
+        if i < len(action_queue):
+            playeractions[i].updateimages(colour_images[action_queue[i]], shape_images[action_queue[i]], icons[action_queue[i]])
+        else:
+            playeractions[i].updateimages("Grey.png", "Blank.png", "Blank.png")
+
+
+
+def  update_enemy_action_display():
+    for i in range(len(playeractions)):
+        if revealed_attacks[i]:
+            enemyactions[i].updateimages(colour_images[enemy.action_queue[i]], shape_images[enemy.action_queue[i]], icons[enemy.action_queue[i]])
+        else:
+            enemyactions[i].updateimages("Grey.png", "Blank.png", "Questionmark.png")
+
+def reveal_enemy_attack(index):
+    global player_reveals
+    if player_reveals > 0:
+        revealed_attacks[index] = True
+        player_reveals -= 1
+        update_enemy_action_display()
+
+
+
+def cancel_action(action_index):
+    print(action_index)
+    if action_index < len(action_queue):
+        del action_queue[action_index]
+        update_player_action_display()
+
+
+def add_action_to_queue(chosen_action):
+    action_queue.append(chosen_action)
+    update_player_action_display()
+
+
+def setup_enemy_fight():
+    global enemy
+    enemy = character(3)
+    setup_round()
+
+
+def setup_round():
+    global enemy
+    global revealed_attacks
+    global player_reveals
+    revealed_attacks = [True, False, False, False, False, False, False]
+    enemy.action_queue = [3, 0, 0, 3, 0, 0, 0]
+    player_reveals = 2
+    update_enemy_action_display()
 
 
 
@@ -75,31 +186,33 @@ def main():
     background.fill((250, 250, 250))
 
     #first sprites
-    playeractions = []
-    for i in range(7):
-        playeractions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
 
-    enemyactions = []
     for i in range(7):
-        enemyactions.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
+        playeractions.append(actiondisplay((0, 0), (100, 100)))
+
+
+    for i in range(7):
+        enemyactions.append(actiondisplay((0, 0), (100, 100)))
 
 
     playerchoices = []
     for i in range(6):
-        playerchoices.append(imagedisplay((0, 0), (100, 100), "Warrior.png"))
+        playerchoices.append(actiondisplay((0, 0), (100, 100)))
+
+
 
 
     playerhp = textdisplay((180, 800), "Player HP: " + "100")
     enemyhp = textdisplay((1400, 800), "Enemy HP: " + "100")
     enemyname = textdisplay((1355, 765), "Gregore")
     playerimage = imagedisplay((180, 680), (200, 200), "Warrior.png")
-    enemyimage = imagedisplay((1400, 680), (200, 200), "Warrior.png")
+    enemyimage = imagedisplay((1400, 680), (200, 200), "GoblinBG.png")
 
     centeraround(playeractions, (screen.get_size()[0]/2, 600), 20)
     centeraround(playerchoices, (screen.get_size()[0]/2, 800), 20)
     centeraround(enemyactions, (screen.get_size()[0]/2, 400), 20)
 
-    spritelist = []
+
     spritelist.append(playerhp)
     spritelist.append(enemyhp)
     spritelist.append(enemyname)
@@ -107,18 +220,34 @@ def main():
     spritelist.append(enemyimage)
 
 
-    for i in playeractions:
-        spritelist.append(i)
-    for i in enemyactions:
-        spritelist.append(i)
-    for i in playerchoices:
-        spritelist.append(i)
-
-    buttonlist = []
-    nextturn = Button((500, 500), (150, 40), "Next turn")
+    nextturn = Button((1450, 850), (150, 40), "Next turn")
     buttonlist.append(nextturn)
+
     nextturn.add_to_render(spritelist)
     nextturn.action = testaction
+
+    for i in playeractions:
+        i.updateposition()
+        i.add_to_render(spritelist)
+        i.action = partial(cancel_action, playeractions.index(i))
+        buttonlist.append(i)
+
+
+    for i in enemyactions:
+        i.updateposition()
+        i.add_to_render(spritelist)
+        i.action = partial(reveal_enemy_attack, enemyactions.index(i))
+        buttonlist.append(i)
+
+
+    for i in playerchoices:
+        i.updateposition()
+        i.add_to_render(spritelist)
+        i.action = partial(add_action_to_queue, playerchoices.index(i))
+        index = playerchoices.index(i)
+        i.updateimages(colour_images[index], shape_images[index], icons[index])
+        buttonlist.append(i)
+
 
     # Display some text
     font = pygame.font.Font(None, 36)
@@ -131,6 +260,7 @@ def main():
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
+    setup_enemy_fight()
 
     # Event loop
     while 1:
@@ -158,4 +288,4 @@ if __name__ == '__main__':
     main()
 
 
-print(__name__)
+
